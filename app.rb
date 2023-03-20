e532699f028b6d646732c1f77520028b4209f3e9
Tommy Le <tommy.le2@gmail.com>
2023-03-19 17:26:50 -0700

    Commit: Add app.js to release archive and update create_release_archive.yml

---
+++ app.rb
@@ -0,0 +1,19 @@
+class commitai < Formula
+    desc "A CLI tool to generate git commit messages using OpenAI's GPT-4."
+    homepage "https://github.com/tommyle/commit_ai_nodejs/tree/main"
+    url "https://github.com/tommyle/commit_ai_nodejs/archive/refs/tags/v0.0.4.tar.gz"
+    sha256 "31841eb4054168fa8ffa601b5988ea1e376e75b4374db980f4b43d8363393da2"
+    license "MIT"
+  
+    depends_on "node"
+  
+    def install
+      libexec.install Dir["*"]
+      bin.install_symlink "#{libexec}/app.js" => "commitai"
+    end
+  
+    test do
+      system "#{bin}/commitai", "--version"
+    end
+  end
+  
