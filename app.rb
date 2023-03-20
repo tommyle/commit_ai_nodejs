e862a061662ae89d2f415fba25a3c2d7cad9901a
Tommy Le <tommy.le2@gmail.com>
2023-03-19 17:46:07 -0700

    Update sha256

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ class App < Formula
     desc "A CLI tool to generate git commit messages using OpenAI's GPT-4."
     homepage "https://github.com/tommyle/commit_ai_nodejs/tree/main"
     url "https://github.com/tommyle/commit_ai_nodejs/archive/refs/tags/v0.0.4.tar.gz"
-    sha256 "31841eb4054168fa8ffa601b5988ea1e376e75b4374db980f4b43d8363393da2"
+    sha256 "f60cb101ebb6e3440abc38b8031639e6ee3d142a5fe8f026b2b633e9c41bf64c"
     license "MIT"
   
     depends_on "node"
